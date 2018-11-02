770c3ee5c480501bfa9a640db13848bb3a9882e4
martinyounghoonkim <hoons0131@gmail.com>
2018-11-02 23:05:18 +0900

    Add header components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" v-model="text"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  data () {
+    return {
+      text: ''
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
+
