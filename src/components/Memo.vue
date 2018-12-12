8982158e9740a821e52e246bc6daa904393b31d8
martinyounghoonkim <hoons0131@gmail.com>
2018-12-12 22:50:10 +0900

    Add editing id in Memo's parent component

--- src/components/Memo.vue
+++ src/components/Memo.vue
@@ -3,11 +3,12 @@
     <strong>{{ memo.title }}</strong>
     <p @dblclick="handleDblClick">
       <template v-if="!isEditing">{{ memo.content }}</template>
-      <textarea v-else
-                ref="content"
-                type="text"
-                :value="memo.content"
-                @keydown.enter="editMemo"/>
+      <input type="text"
+             v-else
+             ref="content"
+             :value="memo.content"
+             @blur="handleBlur"
+             @keydown.enter="editMemo"/>
     </p>
     <div>
       <button type="button" @click="deleteMemo">X버튼</button>
@@ -18,30 +19,36 @@
   export default {
     name: 'Memo',
     props: {
+      editingId: {
+        type: Number,
+      },
       memo: {
         type: Object
       }
     },
-    data () {
-      return {
-        isEditing: false,
+    computed: {
+      isEditing () {
+        return this.memo.id === this.editingId;
       }
     },
     methods: {
+      handleBlur () {
+        this.$emit('endEditing');
+      },
       handleDblClick () {
-        this.isEditing = true;
+        const { id } = this.memo;
+        this.$emit('startEditing', id);
         this.$nextTick(() => {
           this.$refs.content.focus();
         });
       },
       editMemo (e) {
         const id = this.memo.id;
-        const content = e.target.value;
+        const content = e.target.value.trim();
         if (content.length <= 0) {
           return false;
         }
         this.$emit('editMemo', { id, content });
-        this.isEditing = false;
       },
       deleteMemo () {
         const id = this.memo.id;
@@ -92,10 +99,9 @@
   .memo-list p {
     margin: 0;
   }
-  .memo-list p textarea {
+  .memo-list p input[type="text"] {
     box-sizing: border-box;
     width: 100%;
     font-size: inherit;
-    resize: none;
   }
 </style>
