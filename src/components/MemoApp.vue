2ea8441b023f896d0bdd5d4641b54164a9ca5231
martin <martin@tmon.co.kr>
2019-01-11 09:50:13 +0900

    Migrate delete logic to deployd db from localstorage

--- src/components/MemoApp.vue
+++ src/components/MemoApp.vue
@@ -45,8 +45,10 @@ export default {
     },
     deleteMemo (id) {
       const targetIndex = this.memos.findIndex(v => v.id === id);
-      this.memos.splice(targetIndex, 1);
-      this.storeMemo();
+      memoAPICore.delete(`/${id}`)
+        .then(() => {
+          this.memos.splice(targetIndex, 1);
+        });
     },
     editMemo (payload) {
       const { id, content } = payload;
