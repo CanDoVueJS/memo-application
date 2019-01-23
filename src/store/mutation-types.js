80a4332de783521bdf404a4942211009bd28bb03
martinyounghoonkim <hoons0131@gmail.com>
2019-01-24 00:40:59 +0900

    Refactoring vuex code

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,5 +1,4 @@
 export const FETCH_MEMOS = 'FETCH_MEMOS';
-export const SET_MEMO = 'SET_MEMO';
 export const DELETE_MEMO = 'DELETE_MEMO';
 export const UPDATE_MEMO = 'UPDATE_MEMO';
 export const CREATE_MEMO = 'CREATE_MEMO';
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,25 +1,23 @@
-
-export function FETCH_MEMOS (state, payload) {
-  state.memos = payload;
-}
-
-export function CREATE_MEMO (state, payload) {
-  state.memos.push(payload);
-}
-
-export function DELETE_MEMO (state, id) {
-  const targetIndex = state.memos.findIndex(v => v.id === id);
-  state.memos.splice(targetIndex, 1);
-}
-
-export function UPDATE_MEMO (state, payload) {
-  const targetIndex = state.memos.findIndex(v => v.id === payload.id);
-  state.memos.splice(targetIndex, 1, payload);
-}
-
-export default {
-  FETCH_MEMOS,
+import {
   CREATE_MEMO,
   DELETE_MEMO,
+  FETCH_MEMOS,
   UPDATE_MEMO
+} from './mutation-types';
+
+export default {
+  [FETCH_MEMOS] (state, payload) {
+    state.memos = payload;
+  },
+  [CREATE_MEMO] (state, payload) {
+    state.memos.push(payload);
+  },
+  [DELETE_MEMO] (state, id) {
+    const targetIndex = state.memos.findIndex(v => v.id === id);
+    state.memos.splice(targetIndex, 1);
+  },
+  [UPDATE_MEMO] (state, payload) {
+    const targetIndex = state.memos.findIndex(v => v.id === payload.id);
+    state.memos.splice(targetIndex, 1, payload);
+  }
 };
